0777b0221617e5c50aeca954d60829a72706320c
roopekj <40203658+roopekj@users.noreply.github.com>
2019-02-24 13:14:12 +0200

    Add files via upload

---
+++ Sorter.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Sorting_algorithms
+{
+    public partial class Sorter : Form
+    {
+        private Random random = new Random();
+        private List<int[]> values = new List<int[]>();
+        private int frame_index = 0;
+        private static Timer timer = new Timer();
+
+        public Sorter()
+        {
+            InitializeComponent();
+            refresh_values_list();
+            timer = new Timer();
+            timer.Tick += new EventHandler(new_frame);
+            timer.Interval = 1;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineWidth = 0;
+        }
+
+        private void selection_sort(int[] array)
+        {
+            int size = array.Length;
+            refresh_values_list();
+            for (int index = 0; index < size; index++)
+            {
+                values.Add(array.ToArray());
+                int smallest_index = index;
+                for (int x = smallest_index + 1; x < array.Length; x++)
+                {
+                    if (array[x] < array[smallest_index])
+                    {
+                        smallest_index = x;
+                    }
+                }
+                var val = array[index];
+                array[index] = array[smallest_index];
+                array[smallest_index] = val;
+            }
+        }
+
+        public void double_selection_sort(int[] array)  // Deciding the smallest and biggest value in one scan through the array
+        {
+            refresh_values_list();
+            int size = array.Length;
+            int i = 0;
+            int j = size - 1;
+            while (true)
+            {
+                int smallest_index = i;
+                int biggest_index = j;
+                for (int index = i + 1; index <= j; index++)
+                {
+                    if (array[index] < array[smallest_index])
+                    {
+                        smallest_index = index;
+                    }
+                    if (array[index] > array[biggest_index])
+                    {
+                        biggest_index = index;
+                    }
+                }
+
+                var val = array[i];
+                array[i] = array[smallest_index];
+                array[smallest_index] = val;
+                values.Add(array.ToArray());
+
+                if (values.Count == size - 1)
+                {
+                    break;
+                }
+                else if (array[smallest_index] > array[biggest_index])
+                {
+                    biggest_index = smallest_index;
+                }
+                i++;
+
+                var val_ = array[j];
+                array[j] = array[biggest_index];
+                array[biggest_index] = val_;
+                values.Add(array.ToArray());
+
+                if (values.Count == size - 1)
+                {
+                    break;
+                }
+                j--;
+            }
+        }
+
+        public void shaker_sort(int[] array)    //i.e. two-way bubble sort
+        {
+            refresh_values_list();
+            int size = array.Length;
+            while (true)
+            {
+                for (int index = 0; index < array.Length - 1; index++)
+                {
+                    if (array[index] > array[index + 1])
+                    {
+                        var s = array[index + 1];
+                        array[index + 1] = array[index];
+                        array[index] = s;
+                    }
+                }
+                values.Add(array.ToArray());
+
+                if (values.Count == size - 1)
+                {
+                    break;
+                }
+                for (int index_ = array.Length - 1; index_ > 0; index_--)
+                {
+                    if (array[index_] < array[index_ - 1])
+                    {
+                        var s = array[index_ - 1];
+                        array[index_ - 1] = array[index_];
+                        array[index_] = s;
+                    }
+                }
+                values.Add(array.ToArray());
+
+                if (values.Count == size - 1)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void bubble_sort(int[] array)
+        {
+            refresh_values_list();
+            for (int index = array.Length - 1; index > 0; index--)
+            {
+                for (int x = 0; x < index; x++)
+                {
+                    if (array[x + 1] < array[x])
+                    {
+                        var val = array[x + 1];
+                        array[x + 1] = array[x];
+                        array[x] = val;
+                        values.Add(array.ToArray());
+                    }
+                }
+            }
+        }
+
+        private void gnome_sort(int[] array)
+        {
+            refresh_values_list();
+            for (int index = 1; index < array.Length; index++)
+            {
+                for (int x = index - 1; x >= -1; x--)
+                {
+                    if (x == -1)
+                    {
+                        int value = array[index];
+                        for (int shift_index = index; shift_index > 0; shift_index--)
+                        {
+                            array[shift_index] = array[shift_index - 1];
+                            values.Add(array.ToArray());
+                        }
+                        array[0] = value;
+                        break;
+                    }
+                    else if (array[x] < array[index])
+                    {
+                        int value = array[index];
+                        for (int shift_index = index; shift_index > x + 1; shift_index--)
+                        {
+                            array[shift_index] = array[shift_index - 1];
+                            values.Add(array.ToArray());
+                        }
+                        array[x + 1] = value;
+                        values.Add(array.ToArray());
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void insertion_sort(int[] array)
+        {
+            refresh_values_list();
+            for (int index = 1; index < array.Length; index++)
+            {
+                for (int x = 0; x < index; x++)
+                {
+                    if (array[x] > array[index])
+                    {
+                        int value = array[index];
+                        for (int shift_index = index; shift_index > x; shift_index--)
+                        {
+                            array[shift_index] = array[shift_index - 1];
+                            values.Add(array.ToArray());
+                        }
+                        array[x] = value;
+                        values.Add(array.ToArray());
+                    }
+                }
+            }
+        }
+
+        private int sort_range(int start, int end, int[] array)  // Moves all smaller values to the left and bigger values to the right of array[end]. Returns final index of original pivot.
+        {
+            int i = start;
+            int pivot = array[end];
+            for (int index = start; index < end; index++)
+            {
+                if (array[index] < pivot)
+                {
+                    var val = array[index];
+                    array[index] = array[i];
+                    array[i] = val;
+                    i++;
+                    values.Add(array.ToArray());
+                }
+            }
+
+            array[end] = array[i];
+            array[i] = pivot;
+
+            return i;
+        }
+        private void quick_sort(int[] array)
+        {
+            refresh_values_list();
+            pivot(0, array.Length - 1, array);
+        }
+        private void pivot(int start, int end, int[] array)
+        {
+            if (start < end)    // Recursively sorts values to the left of the original array[end] until sort_range returns a value equal to start
+                                // At this point the smallest value has been moved to the beginning of the given array and there are no values left to sort in the first section
+                                // Lastly sorts values to the right of the original array[end]
+            {
+                int i = sort_range(start, end, array);
+                pivot(start, i - 1, array);
+                pivot(i, end, array);
+            }
+        }
+
+        private void new_data() // Pushing new data-points into the chart
+        {
+            int[] data;
+            try
+            {
+                data = values[frame_index];
+                int val = data[0];
+            }
+            catch
+            {
+                return;
+            }
+            chart1.Series["Numbers"].Points.Clear();
+            int i = 0;
+            foreach (int value in data)
+            {
+                chart1.Series["Numbers"].Points.AddXY(i, value);
+                i++;
+            }
+            chart1.Update();
+        }
+        public void new_frame(Object sender, EventArgs e)   // Moves the animation forward by one frame
+        {
+            timer.Stop();
+            new_data();
+            frame_index++;
+            timer.Enabled = true;
+        }
+        private int[] randomized_array(int[] array, Random random)  // Returns the given array in a random order
+        {
+            return array.OrderBy(i => random.Next()).ToArray();
+        }
+        private void refresh_values_list()   // Making the code a bit more readable
+        {
+            values = new List<int[]>();
+        }
+        private void initiate() // A few lines of boilerplate that need to run regardless of the sorting algorithm used
+        {
+            frame_index = 0;
+            timer.Start();
+            Application.DoEvents();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            selection_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bubble_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            insertion_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            quick_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            shaker_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            gnome_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            double_selection_sort(randomized_array(Enumerable.Range(1, 100).ToArray(), random));
+            initiate();
+        }
+    }
+}
---
+++ Sorter.designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Sorting_algorithms
+{
+    partial class Sorter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.BackImageTransparentColor = System.Drawing.Color.White;
+            this.chart1.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.Unscaled;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.AxisX.IsMarksNextToAxis = false;
+            chartArea1.AxisX.LabelStyle.Enabled = false;
+            chartArea1.AxisX.LabelStyle.IsEndLabelVisible = false;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.LineWidth = 0;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.IsLabelAutoFit = false;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.LineWidth = 0;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.BorderColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderWidth = 0;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.BorderColor = System.Drawing.Color.Black;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.White;
+            series1.EmptyPointStyle.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series1.Legend = "Legend1";
+            series1.Name = "Numbers";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(804, 450);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Selection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(251, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Bubble";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(170, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Insertion";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(332, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Quick";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(89, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Shaker";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(170, 37);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Gnome";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(8, 37);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Double";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Sorting Algorithms - Visualized";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
